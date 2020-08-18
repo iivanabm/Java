3094faa97f2af6ad9e1398cde8d3d44b8a344c93
Ivana Marinho <ivanammarinho@hotmail.com>
2020-08-18 13:24:23 -0300

    início de uma api de ordem de serviço

---
+++ OS-api/osapi/OsApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.osapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsApiApplication.class, args);
+	}
+
+}
